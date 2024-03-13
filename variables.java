cde108743ce17ef0ec337fa15c66087b6ee33e81
Anup Dangi <106950757+AnupDangi@users.noreply.github.com>
2024-03-13 14:01:44 +0545

    Delete variables.java

--- variables.java
+++
@@ -1,8 +0,0 @@
-public class variables  {
-   static int a = 10;   
-    public static void main(String[] args) {
-        //literals are  universal or predefined value which does not change at all
-     int a=2;  
-      System.out.println(a);
-}
-}
