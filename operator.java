2666a4ac7519a6ae7fbcbb81542e26bda8706dbb
Anup Dangi <106950757+AnupDangi@users.noreply.github.com>
2024-03-13 14:06:43 +0545

    Delete operator.java

--- operator.java
+++
@@ -1,8 +0,0 @@
-public class operator {
-    public static void main(String[] args) {
-        int a=20;
-        int b=10;
-        System.out.println("Sum ="+(a+b));
-        System.out.println("Subraction ="+(a-b));
-    }
-}
