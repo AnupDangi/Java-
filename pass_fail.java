30b980c802c7e29837c4afa947ead6f0dbf47e9b
Anup Dangi <106950757+AnupDangi@users.noreply.github.com>
2024-03-13 14:06:55 +0545

    Delete pass_fail.java

--- pass_fail.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class pass_fail {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int marks=sc.nextInt();
-        String reportCard =marks>=33? "Pass":"Fail";
-        System.out.println(reportCard);
-    }
-}
